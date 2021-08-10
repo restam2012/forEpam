f46bcd165f78bd90278c3813f1cf209484a6e4de
restam2012 <restam_2012@mail.ru>
2021-08-10 20:25:24 +0300

    Add files via upload

---
+++ module1/Task133.java
@@ -0,0 +1,23 @@
+/*
+* 3. Найти сумму квадратов первых ста чисел.
+*/
+package by.epam.lesson1.cycle;
+
+public class Task133 {
+    public static void main(String[] args) {
+        int val = 100;
+        int result = 0;
+        
+        for(int i = 1; i <= val; i++){
+            result += i * i;
+            
+            if(result < 0) { //Выход, если привышен размер инта.
+            	System.out.printf("You have got used to the int size in %d cycle.", i);
+            	return;
+            }
+            
+        }
+        
+        System.out.println(result);
+    }
+}
