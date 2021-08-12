51b173ffee54161beeb0e8cc6e7d877fb73e6650
restam2012 <restam_2012@mail.ru>
2021-08-12 21:43:54 +0300

    Add files via upload

--- module2/decomposition/Task2411.java
+++ module2/decomposition/Task2411.java
@@ -1,7 +1,8 @@
 /*
  * 11. Написать метод(методы), определяющий, в каком из данных двух чисел больше цифр
  * 
- * Делал метод для нахождения цифр в целых числах. Хотя можно переделать и для double.
+ * Делал метод для нахождения цифр в целых числах. Хотя можно переделать и для double добавив метод, который доумножает 
+ * число до значения, когда остаток от деления на 1 будет = 0.
  * */
 
 package by.epam.lesson2.decomposition;
@@ -9,39 +10,37 @@ package by.epam.lesson2.decomposition;
 import java.util.Scanner;
 
 public class Task2411 {
-    public static void main(String[] args) {
-        int val1;
-        int val2;
-        int sizeOfVal1;
-        int sizeOfVal2;
-        
-        val1 = waitingInteger("first");
-        val2 = waitingInteger("second");
-        
-        sizeOfVal1 = findSizeOfMass(val1);
-        sizeOfVal2 = findSizeOfMass(val2);
-        
-        if(sizeOfVal1 > sizeOfVal2) {
-            System.out.println("Val1 has more numeral then val2");
-        }
-        else if(sizeOfVal1 < sizeOfVal2){
-            System.out.println("Val2 has more numeral then val1");
-        }
-        else {
-            System.out.println("Val1 has equal numeral then val2");
-        }
-    }
+	public static void main(String[] args) {
+		int val1;
+		int val2;
+		int sizeOfVal1;
+		int sizeOfVal2;
 
-    private static int findSizeOfMass(int val){
-        int count = 1;
-        while (val >= 10){
-            val /= 10;
-            count++;
-        }
-        return count;
-    }
-    
-    private static int waitingInteger(String message) {
+		val1 = waitingInteger("first");
+		val2 = waitingInteger("second");
+
+		sizeOfVal1 = findSizeOfMass(val1);
+		sizeOfVal2 = findSizeOfMass(val2);
+
+		if (sizeOfVal1 > sizeOfVal2) {
+			System.out.println("Val1 has more numeral then val2");
+		} else if (sizeOfVal1 < sizeOfVal2) {
+			System.out.println("Val2 has more numeral then val1");
+		} else {
+			System.out.println("Val1 has equal numeral then val2");
+		}
+	}
+
+	private static int findSizeOfMass(int val) {
+		int count = 1;
+		while (Math.abs(val) >= 10) {
+			val /= 10;
+			count++;
+		}
+		return count;
+	}
+
+	private static int waitingInteger(String message) {
 
 		int value = 0;
 
@@ -49,18 +48,14 @@ public class Task2411 {
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.printf("Enter %s int. \n", message);
-		while (value <= 0) {
-			while (!scanner.hasNextInt()) {
-				scanner.next();
-				System.out.println("You enter not int. Try again.");
-				System.out.printf("Enter %s int. \n", message);
-			}
 
-			value = scanner.nextInt();
-			if (value <= 0) {
-				System.out.println("You enter not positive value");
-			}
+		while (!scanner.hasNextInt()) {
+			scanner.next();
+			System.out.println("You enter not int. Try again.");
+			System.out.printf("Enter %s int. \n", message);
 		}
+
+		value = scanner.nextInt();
 
 		return value;
 	}
